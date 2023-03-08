c4c16cb4a40e626782b3761b3597f4aa4c1bf573
abhiyandhakal <itsabhiyandhakal@gmail.com>
2023-03-08 19:07:28 +0545

    added copilot

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -43,4 +43,6 @@ return require('packer').startup(function(use)
 		'neoclide/coc.nvim', branch = 'release',
 		{ run = 'yarn install --frozen-lockfile' }
 	}
+
+	use 'github/copilot.vim'
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -84,6 +84,11 @@ _G.packer_plugins = {
     path = "/home/abhiyan/.local/share/nvim/site/pack/packer/start/coc.nvim",
     url = "https://github.com/neoclide/coc.nvim"
   },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/home/abhiyan/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
+  },
   ["emmet-vim"] = {
     loaded = true,
     path = "/home/abhiyan/.local/share/nvim/site/pack/packer/start/emmet-vim",
