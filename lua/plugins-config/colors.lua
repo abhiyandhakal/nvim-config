1511d3fea92228122706eee730aaa78380df4ad7
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-05-10 20:30:43 +0545

    highlighting border throws error, so removed it

--- lua/plugins-config/colors.lua
+++ lua/plugins-config/colors.lua
@@ -2,8 +2,5 @@ require("catppuccin").setup {
 	transparent_background = true,
 }
 
--- set vertical split border color
-vim.cmd('highlight VertSplit guibg=#555686')
-
 -- Set the colorscheme
 vim.cmd('colorscheme catppuccin')
