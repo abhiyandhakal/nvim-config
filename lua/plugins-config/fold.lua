a6898af874e00b0486cc41f6ae64dfa1052157a6
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-05-10 07:01:22 +0545

    fold column added

---
+++ lua/plugins-config/fold.lua
@@ -0,0 +1,14 @@
+vim.o.foldcolumn = '0'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+require('ufo').setup({
+	provider_selector = function(bufnr, filetype, buftype)
+		return { 'treesitter', 'indent' }
+	end
+})
