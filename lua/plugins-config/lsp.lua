058a960260c5f80f47199a16485a1731e246a30a
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-06-15 16:16:31 +0545

    copilot added, formatting and stuff

--- lua/plugins-config/lsp.lua
+++ lua/plugins-config/lsp.lua
@@ -1,40 +1,39 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-	desc = 'LSP actions',
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
 	callback = function(event)
 		local opts = { buffer = event.buf }
 
 		-- lsp stuff
-		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
-		vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts)
 
 		-- diagnostics stuff
-		vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, opts)
-		vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, opts)
-		vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
-		vim.keymap.set('n', '<leader>dt', '<cmd>Telescope diagnostics<CR>', opts)
-	end
+		vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "<leader>dt", "<cmd>Telescope diagnostics<CR>", opts)
+	end,
 })
 
 -- nvim cmp
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 local has_words_before = function()
 	unpack = unpack or table.unpack
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and
-			vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
+			require("luasnip").lsp_expand(args.body)
 		end,
 	},
 	window = {
@@ -42,11 +41,11 @@ cmp.setup({
 		-- documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		['C-B'] = cmp.mapping.scroll_docs(-4),
-		['C-f'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		["C-B"] = cmp.mapping.scroll_docs(-4),
+		["C-f"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
@@ -69,50 +68,50 @@ cmp.setup({
 		end, { "i", "s" }),
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lua' },
-		{ name = 'nvim_lsp', max_item_count = 1 },
-		{ name = 'luasnip' }
+		{ name = "nvim_lua" },
+		{ name = "nvim_lsp", max_item_count = 10 },
+		{ name = "luasnip" },
 	}, {
-		{ name = 'buffer' }
-	})
+		{ name = "buffer" },
+	}),
 })
 
-cmp.setup.cmdline({ '/', '?' }, {
+cmp.setup.cmdline({ "/", "?" }, {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
-		{ name = 'buffer' }
-	}
+		{ name = "buffer" },
+	},
 })
 
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
-		{ name = 'path' }
+		{ name = "path" },
 	}, {
-		{ name = 'cmdline' }
-	})
+		{ name = "cmdline" },
+	}),
 })
 
-require('mason').setup()
-require('mason-lspconfig').setup({
+require("mason").setup()
+require("mason-lspconfig").setup({
 	ensure_installed = {
-		'lua_ls',
-		'rust_analyzer',
-		'tsserver',
-		'jsonls',
-		'clangd',
-		'html',
-		'pyright',
-		'cssls',
-		'cssmodules_ls',
-		'svelte'
-	}
+		"lua_ls",
+		"rust_analyzer",
+		"tsserver",
+		"jsonls",
+		"clangd",
+		"html",
+		"pyright",
+		"cssls",
+		"cssmodules_ls",
+		"svelte",
+	},
 })
 
-local lspconfig = require('lspconfig')
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require("lspconfig")
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("mason-lspconfig").setup_handlers {
+require("mason-lspconfig").setup_handlers({
 	function(server_name)
 		lspconfig[server_name].setup({
 			capabilities = lsp_capabilities,
@@ -123,12 +122,13 @@ require("mason-lspconfig").setup_handlers {
 		settings = {
 			diagnostics = {
 				globals = {
-					'vim', 'require'
-				}
-			}
-		}
+					"vim",
+					"require",
+				},
+			},
+		},
 	}),
-}
+})
 
 local rt = require("rust-tools")
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,9 @@
 local ensure_packer = function()
 	local fn = vim.fn
-	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ 'git', 'clone', '--depth', '1',
-			'https://github.com/wbthomason/packer.nvim', install_path })
-		vim.cmd [[packadd packer.nvim]]
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
 		return true
 	end
 	return false
@@ -12,111 +11,108 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use("wbthomason/packer.nvim")
 
 	-- comment using gcc and gc
-	use {
-		'numToStr/Comment.nvim',
+	use({
+		"numToStr/Comment.nvim",
 		config = function()
-			require('Comment').setup()
-		end
-	}
+			require("Comment").setup()
+		end,
+	})
 
 	-- css colors highlighting
-	use 'ap/vim-css-color'
+	use("ap/vim-css-color")
 
 	-- colorscheme
 	-- use 'rose-pine/neovim'
-	use 'catppuccin/nvim'
+	use("catppuccin/nvim")
 
 	-- icons
-	use 'ryanoasis/vim-devicons'
+	use("ryanoasis/vim-devicons")
 
 	-- indent lines
-	use 'lukas-reineke/indent-blankline.nvim'
+	use("lukas-reineke/indent-blankline.nvim")
 
 	-- telescope
-	use {
-		'nvim-telescope/telescope.nvim', branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
+	use({
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
 	-- git integration
-	use 'tpope/vim-fugitive'
+	use("tpope/vim-fugitive")
 
 	-- treesitter
-	use(
-		'nvim-treesitter/nvim-treesitter',
-		{ run = ':TSUpdate' }
-	)
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
 	-- file browser
-	use {
-		'nvim-neo-tree/neo-tree.nvim',
-		branch = 'v2.x',
+	use({
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
 		requires = {
-			'nvim-lua/plenary.nvim',
-			'nvim-tree/nvim-web-devicons',
-			'MunifTanjim/nui.nvim',
-		}
-	}
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+	})
 
 	-- bracket pairing
-	use 'LunarWatcher/auto-pairs'
+	use("LunarWatcher/auto-pairs")
 
 	-- discord presence
-	use 'andweeb/presence.nvim'
+	use("andweeb/presence.nvim")
 
 	-- code folding
-	use 'anuvyklack/pretty-fold.nvim'
+	use("anuvyklack/pretty-fold.nvim")
 
 	-- lsp
-	use "williamboman/mason.nvim"
-	use "williamboman/mason-lspconfig.nvim"
-	use "neovim/nvim-lspconfig"
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/cmp-nvim-lua'
-	use 'L3MON4D3/LuaSnip'
-	use "rafamadriz/friendly-snippets"
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-nvim-lua")
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
 
-	use 'saadparwaiz1/cmp_luasnip'
+	use("saadparwaiz1/cmp_luasnip")
 
 	-- debugging tools
-	use 'mfussenegger/nvim-dap'
+	use("mfussenegger/nvim-dap")
 
 	-- rust related
-	use 'simrat39/rust-tools.nvim'
+	use("simrat39/rust-tools.nvim")
 
 	-- for diagnostics
-	use {
+	use({
 		"folke/trouble.nvim",
 		requires = "nvim-tree/nvim-web-devicons",
 		config = function()
-			require("trouble").setup {
-			}
-		end
-	}
+			require("trouble").setup({})
+		end,
+	})
 
 	-- formatting
-	use 'jose-elias-alvarez/null-ls.nvim'
+	use("jose-elias-alvarez/null-ls.nvim")
 
 	-- beautiful status bar with lualine
-	use 'nvim-lualine/lualine.nvim'
+	use("nvim-lualine/lualine.nvim")
 
 	-- terminal
-	use 'vimlab/split-term.vim'
+	use("vimlab/split-term.vim")
 
 	-- startup screen
-	use {
+	use({
 		"startup-nvim/startup.nvim",
 		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-	}
+	})
 
 	-- brackets surrounding and all
 	use({
@@ -124,20 +120,25 @@ return require('packer').startup(function(use)
 		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
 		config = function()
 			require("nvim-surround").setup({})
-		end
+		end,
 	})
 
 	-- harpoon for alternate files
-	use 'ThePrimeagen/harpoon'
+	use("ThePrimeagen/harpoon")
 
 	-- oil.nvim
-	use {
-		'stevearc/oil.nvim',
-		config = function() require('oil').setup() end
-	}
+	use({
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup()
+		end,
+	})
+
+	-- copilot
+	use("zbirenbaum/copilot.lua")
 
 	-- automatically syncs on startup
 	if packer_bootstrap then
-		require('packer').sync()
+		require("packer").sync()
 	end
 end)
