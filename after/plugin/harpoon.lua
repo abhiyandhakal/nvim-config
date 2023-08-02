626d63e528ec21a62697f907ff33e3638c2899ee
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-08-02 17:46:14 +0545

    added harpoon

---
+++ after/plugin/harpoon.lua
@@ -0,0 +1,29 @@
+-- add file
+vim.keymap.set('n', '<leader>a', function()
+  vim.api.nvim_command('lua require("harpoon.mark").add_file()')
+end)
+
+-- toggle quick menu
+vim.keymap.set('n', '<leader>s', function()
+  vim.api.nvim_command('lua require("harpoon.ui").toggle_quick_menu()')
+end)
+
+require("telescope").load_extension('harpoon')
+
+-- navigation
+vim.keymap.set('n', '<Tab>', function()
+  vim.api.nvim_command('lua require("harpoon.ui").nav_next()')
+end)
+vim.keymap.set('n', '<S-Tab>', function()
+  vim.api.nvim_command('lua require("harpoon.ui").nav_prev()')
+end)
+
+vim.keymap.set('n', '<leader>1', function()
+  vim.api.nvim_command('lua require("harpoon.ui").nav_file(1)')
+end)
+vim.keymap.set('n', '<leader>2', function()
+  vim.api.nvim_command('lua require("harpoon.ui").nav_file(2)')
+end)
+vim.keymap.set('n', '<leader>3', function()
+  vim.api.nvim_command('lua require("harpoon.ui").nav_file(3)')
+end)
