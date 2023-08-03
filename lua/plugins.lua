115eba1a846183fb6b04518632917710eab77013
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-08-03 06:38:38 +0545

    added live server

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -142,5 +142,14 @@ require("lazy").setup({
 	{ 'ThePrimeagen/harpoon' },
 
 	-- copilot
-	{ "zbirenbaum/copilot.lua" }
+	{ "zbirenbaum/copilot.lua" },
+
+	-- live server
+	{
+		"aurum77/live-server.nvim",
+		run = function()
+			require("live_server.util").install()
+		end,
+		cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
+	},
 }, {})
