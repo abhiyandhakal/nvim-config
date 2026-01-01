4d2bbf49c17147eb060e6670bcdad18d6ae19540
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2026-01-01 12:59:48 +0545

    fix(treesitter): use autocmd with pcall for built-in treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,15 +1,6 @@
-require 'nvim-treesitter.configs'.setup {
-	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c", "lua", "vim", "query", "javascript", "typescript", "markdown_inline", "rust", "latex" },
-
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
-
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = false,
-
-	highlight = {
-		enable = true,
-	},
-}
+-- Enable treesitter highlighting automatically for supported filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
+})
