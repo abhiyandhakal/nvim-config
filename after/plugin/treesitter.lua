4e17c4d384e84daded89930ea32dbc789502473b
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2026-01-01 12:44:23 +0545

    fix(treesitter): disable auto_install without tree-sitter CLI

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,13 +1,13 @@
 require 'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c", "lua", "vim", "query", "javascript", "typescript", "markdown_inline", "rust" },
+	ensure_installed = { "c", "lua", "vim", "query", "javascript", "typescript", "markdown_inline", "rust", "latex" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
 
 	-- Automatically install missing parsers when entering buffer
 	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
+	auto_install = false,
 
 	highlight = {
 		enable = true,
